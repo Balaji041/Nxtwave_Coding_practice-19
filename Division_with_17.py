5d566f31c07f501aa8e271bafa3c9e9eef3649b4
balaji <76476394+Balaji041@users.noreply.github.com>
2023-09-06 23:04:44 +0530

    Create Division_with_17.py

---
+++ Division_with_17.py
@@ -0,0 +1,14 @@
+N=int(input())
+for i in range(N):
+    num=int(input())
+    print(num//17) #using floor division operator for display only integral part
+"""
+input:3
+5
+98
+376
+output:
+0
+5
+22
+"""
